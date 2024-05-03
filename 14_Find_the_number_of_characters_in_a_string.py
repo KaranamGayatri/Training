33445c8b5aad6a46ae5004ad9f3ff36e3a181661
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-03 15:25:23 +0530

    Number of chracters in string

---
+++ 14_Find_the_number_of_characters_in_a_string.py
@@ -0,0 +1,5 @@
+#Number of chracters in string
+string = input("Enter a string: ")
+characters = len(string)
+print ("The string is ",string)
+print ("The number of characters in the string is: ",characters)
