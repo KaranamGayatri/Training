f53284d398af429e4918396045accfb75cf9dbdb
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-06 15:35:47 +0530

    Use str() to concatenate

--- 18_type_str_and_escape.py
+++ 18_type_str_and_escape.py
@@ -3,3 +3,7 @@ Float = 56.7
 
 #Use print() and type() to print the data type of the variable in the output
 print(Float,type(Float))
+
+#Use str() on the variable from step 1 and concatenate it with the string " is a float." then use print() to display the result
+result = str(Float) + " is a float."
+print(result)
