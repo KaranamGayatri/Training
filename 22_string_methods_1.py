f95f2dbc7076e34553efbd6824b84b507c07196f
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-06 16:03:43 +0530

    Using .isupper()

--- 22_string_methods_1.py
+++ 22_string_methods_1.py
@@ -1,2 +1,5 @@
 #Create a variable called mixed_case
 mixed_case = "A Song of Ice and Fire"
+
+#Use .isupper() to check if mixed_case is a string of all upper case letters
+print(mixed_case.isupper())
