90de14b457087b374e004c51da1d1128c68188ef
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-06 15:39:21 +0530

    print() the string including quotes

--- 18_type_str_and_escape.py
+++ 18_type_str_and_escape.py
@@ -7,3 +7,6 @@ print(Float,type(Float))
 #Use str() on the variable from step 1 and concatenate it with the string " is a float." then use print() to display the result
 result = str(Float) + " is a float."
 print(result)
+
+#print() the string "Hello, I'm [name], it's nice to meet you!"
+print("\"Hello, I'm Gayatri, it's nice to meet you!\"")
