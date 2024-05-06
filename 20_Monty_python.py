f58d2f0ceb3e0dee4e6fd4e20423330228ac82fe
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-06 15:51:34 +0530

    Monty Python

---
+++ 20_Monty_python.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+quest = input("What is your quest? ")
+color = input("What is your favorite color? ")
+
+String = "So your name name is {}, your quest is {}, and your favorite color is {}.".format(name, quest, color)
+print(String)
