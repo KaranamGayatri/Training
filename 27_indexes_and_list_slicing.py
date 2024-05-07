ff3c387520d1ac723a13729c4ec2f3e9fca7fc4e
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-07 19:21:39 +0530

    Access the first list from the list of lists

--- 27_indexes_and_list_slicing.py
+++ 27_indexes_and_list_slicing.py
@@ -1,2 +1,6 @@
 #Create a variable and assign it the list [[0, 2], [4, 6], [8, 10], [12, 14]]
 List = [[0, 2], [4, 6], [8, 10], [12, 14]]
+
+#Access the first list from the list of lists in step 1 by index then print it
+first_list = List[0]
+print(first_list)
