e514f60569dbb16a08e437d2287e4c36cab9fc3b
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-07 16:21:11 +0530

    Call .ljust() on the_string with the arguments 17 and *

--- 23_string_methods_2.py
+++ 23_string_methods_2.py
@@ -4,3 +4,5 @@ the_string = "North Dakota"
 #Call .rjust() on the_string with 17 as its argument and print() the result.
 print(the_string.rjust(17))
 
+#Call .ljust() on the_string with the arguments 17 and "*" then print() the result.
+print(the_string.ljust(17,"*"))
