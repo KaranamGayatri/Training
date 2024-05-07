cadf1a4f134808e82ebc026ccdaf7ef7cbb34700
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-07 18:12:46 +0530

    Create another variable and assign it a call of the list() function with a string as its argument

--- 26_lists_exercises.py
+++ 26_lists_exercises.py
@@ -1,2 +1,5 @@
 #Create a variable and assign it a list that contains an integer, a float, a Boolean value, a string, and a list of 3 integers.
 List = [11, 5.13, True, "hi", [4, 5, 7]]
+
+#Create another variable and assign it a call of the list() function with a string as its argument
+Second_List = list("example")
