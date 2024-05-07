e6622c0f2dc981247de319e909951d01309adabf
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-07 19:24:51 +0530

    Access the 14 from the list in step 1 then print it

--- 27_indexes_and_list_slicing.py
+++ 27_indexes_and_list_slicing.py
@@ -4,3 +4,6 @@ List = [[0, 2], [4, 6], [8, 10], [12, 14]]
 #Access the first list from the list of lists in step 1 by index then print it
 first_list = List[0]
 print(first_list)
+
+#Access the 14 from the list in step 1 then print it
+print(List[3][1])
