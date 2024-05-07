b698d5a5baa91c20542d4ed5db88d5b2d2c6fd29
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-07 19:08:47 +0530

    Use the keyword in to check if the letter e is in the list

--- 26_lists_exercises.py
+++ 26_lists_exercises.py
@@ -1,5 +1,8 @@
 #Create a variable and assign it a list that contains an integer, a float, a Boolean value, a string, and a list of 3 integers.
-List = [11, 5.13, True, "hi", [4, 5, 7]]
+List = [11, 5.13, True, "pyhton", [4, 5, 7]]
 
 #Create another variable and assign it a call of the list() function with a string as its argument
-Second_List = list("example")
+Second_List = list("exercise")
+
+#Use the keyword "in" to check if the letter "e" is in the list assigned to the variable
+print("e" in Second_List)
