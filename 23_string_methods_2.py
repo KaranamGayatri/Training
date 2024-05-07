1909abf921704fbba2cd5e9efc8fb151bb030db7
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-07 16:18:41 +0530

    Call .rjust() on the_string with 17

--- 23_string_methods_2.py
+++ 23_string_methods_2.py
@@ -1,3 +1,6 @@
 #Create a variable called the_string and assign it the string "North Dakota"
 the_string = "North Dakota"
 
+#Call .rjust() on the_string with 17 as its argument and print() the result.
+print(the_string.rjust(17))
+
