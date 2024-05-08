0a62a749195ce59cf4736f6cd2336c03a8852590
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-08 12:32:15 +0530

    print and access the value held at the third key in the dictionary

--- 29_dictionaries_exercises.py
+++ 29_dictionaries_exercises.py
@@ -1,2 +1,5 @@
 #Create a variable and assign it a dictionary that has 5 key value pairs
 dict = {"one": 1, "two": 2, "three": 3, "four": 4}
+
+#print and access the value held at the third key in the dictionary
+print(dict["three"])
