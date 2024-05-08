b226a8f0d2edd8219d865ed7283eab05d5c38b83
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-08 13:13:18 +0530

    pop and print Big Mac

--- 31_dictionary_methods_2.py
+++ 31_dictionary_methods_2.py
@@ -1,3 +1,8 @@
 #use .fromkeys() to create a dictionary
 for key, value in {}.fromkeys("bcdefghijklmnopqrstuvwxyz","consonant").items():
     print(key, value)
+
+#pop and print "Big Mac"
+fast_food_items = {"McDonald's": "Big Mac", "Burger King": "Whopper", "Chick-fil-A": "Original Chicken Sandwich"}
+print(fast_food_items.pop("McDonald's"))
+ 
