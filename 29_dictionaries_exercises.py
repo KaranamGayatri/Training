86ae2db96c0811b5d0c7e27beaa4e7abc9cd1624
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-08 12:33:43 +0530

    use the in keyword to check if a key appears in the dictionary and print the result

--- 29_dictionaries_exercises.py
+++ 29_dictionaries_exercises.py
@@ -3,3 +3,6 @@ dict = {"one": 1, "two": 2, "three": 3, "four": 4}
 
 #print and access the value held at the third key in the dictionary
 print(dict["three"])
+
+#use the in keyword to check if a key appears in the dictionary and print the result
+print("one" in dict)
