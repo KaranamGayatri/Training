c23719f148e1e0408b9dd4472484ffacd9ed8adb
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-08 12:43:54 +0530

    make the dictionary span multiple lines so that the line the dictionary starts on is not too long

--- 30_dictionary_methods_1.py
+++ 30_dictionary_methods_1.py
@@ -1,2 +1,10 @@
 #create a variable and assign
 songs = {"Queen": "Bohemian Rhapsody", "Bee Gees": "Stayin' Alive", "U2": "One", "Michael Jackson": "Billie Jean", "The Beatles": "Hey Jude", "Bob Dylan": "Like A Rolling Stone"}
+
+#make the dictionary span multiple lines so that the line the dictionary starts on is not too long
+songs = {"Queen": "Bohemian Rhapsody", 
+         "Bee Gees": "Stayin' Alive", 
+         "U2": "One", 
+         "Michael Jackson": "Billie Jean", 
+         "The Beatles": "Hey Jude", 
+         "Bob Dylan": "Like A Rolling Stone"}
