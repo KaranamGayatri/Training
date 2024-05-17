0c9f09310e03d9b011f66b92468714cdafda5a92
KaranamGayatri <ksagayatri99@gmail.com>
2024-05-17 10:06:33 +0530

    From heaven to earth

--- ELEVSTRS.py
+++ ELEVSTRS.py
@@ -1,17 +1,9 @@
-import math
-
-def minimize_travel_time(T, test_cases):
-    results = []
-    for case in test_cases:
-        N, V1, V2 = case
-        time_elevator = 2 * N / V2
-        time_stairs = N * math.sqrt(2) / V1
-        if time_elevator < time_stairs:
-            results.append("Elevator")
-        else:
-            results.append("Stairs")
-    return results
-
-T = 3
-test_cases = [(5, 10, 15), (2, 10, 14), (7, 14, 10)]
-print(minimize_travel_time(T, test_cases))
+T = int(input())
+for i in range(T):
+    N,V1,V2 = map(int,input().split())
+    Elevator = V1*2**0.5/(N)
+    Stairs = V2/N
+    if Elevator<Stairs:
+        print("Elevator")
+    else:
+        print("Stairs")
